ebfe2bd95d5bcea5e07280f6b0e3361ea796a46a
TheMatiaz0 <TheMatiaz0@protonmail.com>
2024-10-11 15:05:58 +0200

    Clean up

--- Assets/Plugins/HoneyAttributes-main/Assets/Plugins/Honey/Dummies/ExternalInt.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace System.Runtime.CompilerServices
-{
-    //https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
-    internal static class IsExternalInit {}
-}
--- Assets/Plugins/RubinPack-main/README.md
+++
@@ -1,5 +0,0 @@
-# Rubin
-
-Base utility stuff for new unity projects. 
-If this was not linked to you i don't recommend using this.
-This is neither documented, nor super useful.
