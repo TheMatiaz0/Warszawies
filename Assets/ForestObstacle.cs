e4e1d139a8cdc2bd61fcb1122a840e3e745785d9
dragondale <dragondalegame@gmail.com>
2024-10-12 18:20:38 +0200

    Add tile assets and make prefabs

--- Assets/ForestObstacle.cs
+++ Assets/ForestObstacle.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ForestObstacle : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject Trees;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Trees.transform.rotation = Quaternion.Euler(0, 45 * (int)Random.Range(0, 8), 0);
     }
 
     // Update is called once per frame
