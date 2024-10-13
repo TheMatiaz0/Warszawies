daac3b0367d42c0aa3e2fab310e4d1018bd15bff
TheMatiaz0 <TheMatiaz0@protonmail.com>
2024-10-13 02:08:25 +0200

    Set 65 as max distance to pivot point

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -76,8 +76,6 @@ public class Player : MonoBehaviour
     {
         var resourceHud = ResourceHuds.Find(x => x.ResourceType == countableResource.ResourceType);
 
-        Debug.Log(BuildingManager.GetIdleCount(countableResource.ResourceType));
-
         resourceHud.Refresh(countableResource.Count, BuildingManager.GetIdleCount(countableResource.ResourceType));
     }
 
